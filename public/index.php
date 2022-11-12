3f6afa499b9e2f3f7c33e70455475b20fa7880ae
Vinicius Dias <carlosv775@gmail.com>
2022-11-12 19:58:19 -0300

    Video 4.1

--- public/index.php
+++ public/index.php
@@ -12,6 +12,8 @@ $pdo = new PDO("sqlite:$dbPath");
 $videoRepository = new VideoRepository($pdo);
 
 $routes = require_once __DIR__ . '/../config/routes.php';
+/** @var \Psr\Container\ContainerInterface $diContainer */
+$diContainer = require_once __DIR__ . '/../config/dependencies.php';
 
 $pathInfo = $_SERVER['PATH_INFO'] ?? '/';
 $httpMethod = $_SERVER['REQUEST_METHOD'];
@@ -28,7 +30,7 @@ $key = "$httpMethod|$pathInfo";
 if (array_key_exists($key, $routes)) {
     $controllerClass = $routes["$httpMethod|$pathInfo"];
 
-    $controller = new $controllerClass($videoRepository);
+    $controller = $diContainer->get($controllerClass);
 } else {
     $controller = new Error404Controller();
 }
